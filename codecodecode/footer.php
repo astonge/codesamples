22e3c6212d6f870fe7d3e0cfa3ce28db59c0605d
astonge <astonge@infotel.ca>
2016-02-11 09:04:33 -0800

    moved wp themes out of this repo

--- codecodecode/footer.php
+++
@@ -1,8 +0,0 @@
-
-<script src="<?php bloginfo('template_url');?>/js/vendor/jquery.js"></script>
-<script src="<?php bloginfo('template_url');?>/js/foundation.min.js"></script>
-<script>
-  $(document).foundation();
-</script>
-  </body>
-</html>
--- lcd/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="footer">
-<h5>&copy<?php echo date('Y');?> adrian st. onge</h5>
-</div>
-</div>
-</body>
-</html>
